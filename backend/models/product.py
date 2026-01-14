51379e5ec38b61834d90213d30e5662f9112e279
tobias <tobias.omondi@student.moringaschool.com>
2026-01-14 12:07:17 +0300

    virtual enviroment +

---
+++ backend/models/product.py
@@ -0,0 +1,17 @@
+# products table model
+from sqlachmemy import Column, Integer, String, Float, DateTime, Boolean
+from database import Base
+from datetime import datetime
+
+class product(Base):
+  __tablename__ = "products"
+  id =Column(Integer, primary_key =True, index = True)
+  name = Column (String, index = True)
+  image_url = Column(String)
+  description = Column (String)
+  price = Column(Float) 
+  stock_quantity = Column (Integer)
+  is_active = Column (Boolean, default = True)
+  created_at = Column (DateTime, default = datetime.utcnow)
+  updated_at = Column (DateTime, default = datetime.utcnow, onupdate=datetime.utcnow)
+
